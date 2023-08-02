d4a4ceae9b854fb4619c21bae58711f6c37ec184
sureface <jasurdjuraev2002@gmail.com>
2023-08-02 13:40:48 +0300

    animation added

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const loader = document.querySelector('.loader');
-let mainHeader = document.querySelector('.main-header')
+// let mainHeader = document.querySelector('.main-header')
 
-window.onscroll = () => {
-    let top = window.scrollY;
-    let offset = main.offsetTop - 400;
-    let height = main.offsetHeight;
+// window.onscroll = () => {
+//     let top = window.scrollY;
+//     let offset = main.offsetTop - 400;
+//     let height = main.offsetHeight;
 
-    if(top >= offset && top < offset + height) {
-        main.classList.add('animate__animated')
-    } else {
-        main.classList.remove('animate__animated')
-    }
-}
+//     if(top >= offset && top < offset + height) {
+//         main.classList.add('animate__animated')
+//     } else {
+//         main.classList.remove('animate__animated')
+//     }
+// }
 
 const removePreloader = () => preloader.remove();
   let preloader = loader
